bc7b2242a69a1e53fc8a71fb893d5bc8d4545a53
kyriekevin <2718629413@qq.com>
2024-07-04 17:37:48 +0800

    add Dive into Deep Learning Ch4 code

---
+++ d2l/code/Ch4/2.parameters.py
@@ -0,0 +1,88 @@
+import torch
+from torch import nn
+from icecream import ic
+
+net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 1))
+X = torch.rand(size=(2, 4))
+net(X)
+
+ic(net[2].state_dict())
+
+ic(type(net[2].bias))
+ic(net[2].bias)
+ic(net[2].bias.data)
+
+net[2].weight.grad == None
+
+ic(*[[name, param.shape] for name, param in net[0].named_parameters()])
+ic(*[[name, param.shape] for name, param in net.named_parameters()])
+
+net.state_dict()['2.bias'].data
+
+def block1():
+    return nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 4), nn.ReLU())
+
+def block2():
+    net = nn.Sequential()
+    for i in range(4):
+        net.add_module(f'block {i}', block1())
+    return net
+
+rgnet = nn.Sequential(block2(), nn.Linear(4, 1))
+rgnet(X)
+
+rgnet
+
+rgnet[0][1][0].bias.data
+
+def init_normal(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, mean=0, std=0.01)
+        nn.init.zeros_(m.bias)
+
+net.apply(init_normal)
+net[0].weight.data[0], net[0].bias.data[0]
+
+def init_constant(m):
+    if type(m) == nn.Linear:
+        nn.init.constant_(m.weight, 1)
+        nn.init.zeros_(m.bias)
+
+net.apply(init_constant)
+net[0].weight.data[0], net[0].bias.data[0]
+
+def xavier(m):
+    if type(m) == nn.Linear:
+        nn.init.xavier_uniform_(m.weight)
+
+def init_42(m):
+    if type(m) == nn.Linear:
+        nn.init.constant_(m.weight, 42)
+
+net[0].apply(xavier)
+net[2].apply(init_42)
+print(net[0].weight.data[0])
+print(net[2].weight.data)
+
+def my_init(m):
+    if type(m) == nn.Linear:
+        print(
+            "Init",
+            *[(name, param.shape) for name, param in m.named_parameters()][0])
+        nn.init.uniform_(m.weight, -10, 10)
+        m.weight.data *= m.weight.data.abs() >= 5
+
+net.apply(my_init)
+net[0].weight[:2]
+
+net[0].weight.data[:] += 1
+net[0].weight.data[0, 0] = 42
+net[0].weight.data[0]
+
+shared = nn.Linear(8, 8)
+net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), shared, nn.ReLU(), shared,
+                    nn.ReLU(), nn.Linear(8, 1))
+net(X)
+print(net[2].weight.data[0] == net[4].weight.data[0])
+net[2].weight.data[0, 0] = 100
+print(net[2].weight.data[0] == net[4].weight.data[0])
